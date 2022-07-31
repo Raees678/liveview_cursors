9496c890bb487e1eb89004f74acaa3e69af4d760
Raees Rajwani <raeesrajwani95@gmail.com>
2022-07-31 19:18:06 +0530

    Add libcluster and vm args for naming the node in production

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule LiveviewCursors.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
+      {:libcluster, "~> 3.3"}
     ]
   end
 
